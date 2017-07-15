d1e7a4a6457b8606773f80d460afd1f0728e77d1
howther <atsukiexfeel@yahoo.co.jp>
2017-07-15 10:57:25 +0900

    アズラエルを修正

--- Azrael.cs
+++ Azrael.cs
@@ -1,12 +1,12 @@
 // 防術機アズラエル用スクリプト
-// エネルギーマネージャのみ試験的に実装
+// Armoriser3.csを参考にさせていただきました。さくさく様に感謝
+// 作成者:江ノ宮（howther111）
 
 using UnityEngine;
 
-public class Azrael : UserScript
-{
-	// 攻撃検出用マスク
-	const int MASK_BULLET = 1;  /// 通常弾(Cannon)
+public class Azrael : UserScript {
+    // 攻撃検出用マスク
+    const int MASK_BULLET = 1;  /// 通常弾(Cannon)
 	const int MASK_SHELL = 2;   /// 戦車砲弾(Cannon)
 	const int MASK_GRENADE = 4; /// 榴弾(Cannon)
 	const int MASK_BLADE = 8;   /// 刃(Sword)
@@ -18,77 +18,79 @@ public class Azrael : UserScript
     bool spin = false;
     int missileMode = 1;
 
+    //アサイン関係
+    KeyCode Wep1 = KeyCode.Mouse0; //マシンガン射撃
+    KeyCode SwordOn = KeyCode.R; //ビームサイスON・OFF
+    KeyCode SwordSlash = KeyCode.Q; //ビームサイス斬撃
+    KeyCode SwordSpin = KeyCode.E; //ビームサイス回転
+    KeyCode Wep2 = KeyCode.Mouse2;
+    KeyCode Missile = KeyCode.Keypad6;
+    KeyCode ChangeOptKey = KeyCode.Keypad5;
+    KeyCode LauncherKey = KeyCode.Keypad4;
+    KeyCode AimAssist = KeyCode.Alpha2;
+    KeyCode AimKey = KeyCode.T;
+    KeyCode BoostKey = KeyCode.F;
+    KeyCode MoveF = KeyCode.W;
+    KeyCode MoveB = KeyCode.S;
+    KeyCode MoveU = KeyCode.Space;
+    KeyCode MoveD = KeyCode.LeftShift;
+    KeyCode MoveL = KeyCode.A;
+    KeyCode MoveR = KeyCode.D;
+
     //----------------------------------------------------------------------------------------------
     // ユーザー名取得
     //----------------------------------------------------------------------------------------------
-    public override string GetUserName()
-	{
-		return "howther111";
-	}
+    public override string GetUserName() {
+        return "howther111";
+    }
 
-	//----------------------------------------------------------------------------------------------
-	// 開始処理
-	//----------------------------------------------------------------------------------------------
-	public override void OnStart(AutoPilot ap)
-	{
+    //----------------------------------------------------------------------------------------------
+    // 開始処理
+    //----------------------------------------------------------------------------------------------
+    public override void OnStart(AutoPilot ap) {
 
-	}
+    }
 
-	//----------------------------------------------------------------------------------------------
-	// 更新処理
-	//----------------------------------------------------------------------------------------------
-	public override void OnUpdate(AutoPilot ap)
-	{
-		// 攻撃
-		int energy = ap.GetEnergy();
+    //----------------------------------------------------------------------------------------------
+    // 更新処理
+    //----------------------------------------------------------------------------------------------
+    public override void OnUpdate(AutoPilot ap) {
+        // 攻撃
+        int energy = ap.GetEnergy();
 
         //銃
-		if(energy > 10 && Input.GetMouseButton(0)) {
+        if (energy > 10 && Input.GetKey(Wep1)) {
             ap.StartAction("ATK1", -1);
         } else {
             ap.EndAction("ATK1");
         }
 
         //ソード
-        if (energy > 30 && (Input.GetKeyDown(KeyCode.R) || Input.GetKeyDown(KeyCode.Q)) && !sword) {
+        if (energy > 30 && (Input.GetKeyDown(SwordOn) || Input.GetKeyDown(SwordSlash)) && !sword) {
             sword = true;
-        } else if (Input.GetKeyDown(KeyCode.R) && sword) {
+        } else if (Input.GetKeyDown(SwordOn) && sword) {
             sword = false;
         }
         if (sword && energy > 10 && !spin) {
             ap.StartAction("ATK3BF", 1);
         }
 
-        if (energy > 30 && !spin && Input.GetKeyDown(KeyCode.E)) {
+        if (energy > 30 && !spin && Input.GetKeyDown(SwordSlash)) {
             ap.StartAction("spin", -1);
             sword = false;
             spin = true;
-        } else if (spin && (energy < 10 || sword || Input.GetKeyDown(KeyCode.E))) {
+        } else if (spin && (energy < 10 || sword || Input.GetKeyDown(SwordSlash))) {
             ap.EndAction("spin");
             spin = false;
         }
 
         //ミサイル
-        if (!missile && energy > 15 && Input.GetMouseButtonDown(1)) {
+        if (!missile && energy > 15 && Input.GetKeyDown(Wep2)) {
             ap.StartAction("ATK2", -1);
-            if (missileMode == 1) {
-                ap.StartAction("ATK2-1", -1);
-            } else if (missileMode == 2) {
-                ap.StartAction("ATK2-2", -1);
-            }
             missile = true;
-        } else if ((missile && Input.GetMouseButtonDown(1)) || energy < 10) {
+        } else if ((missile && Input.GetKeyDown(Wep2)) || energy < 10) {
             ap.EndAction("ATK2");
-            ap.EndAction("ATK2-1");
-            ap.EndAction("ATK2-2");
             missile = false;
-        }
-
-        //ミサイル切り替え
-        if (Input.GetMouseButtonDown(2) && missileMode == 1 && !missile) {
-            missileMode = 2;
-        } else if (Input.GetMouseButtonDown(2) && missileMode == 2 && !missile) {
-            missileMode = 1;
         }
     }
 }
